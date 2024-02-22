a3bee6369fddecf93c423b3e14fd8afb05162d38
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-02-22 20:35:45 +0100

    adding media querys

--- src/Views/myStory.view.php
+++ src/Views/myStory.view.php
@@ -2,31 +2,31 @@
 <?php require __DIR__ . '/partials/navbar.view.php'; ?>
 
 <div class="story-container">
-    <section>
-        <h1><span>A life in the shadows</span></h1>
-        <p>
-            <span>
+    <section class="divider">
+    <span><h1 class="story-header default-font">A life in the shadows</h1></span>
+        <span class="story-text secondary-font">
+            <p>
                 About 20 years. Thats how long I have worked as a nurse in the hospital by night.
                 There have been ups and there have been downs. From the beginning as a novice learning the trade,
                 To big anticlimax that was the global pandemic. I've pushed myself through one of the toughest further education
                 there is for a nurse in sweden and became an specialist nurse in anesthesia 2016.  
-            </span>
+            </p>
 
-            <span>
+            <p>
                 Always awake when everybody else was asleep. Butchering my own social-life in the progress. 
                 Now I've come to the conclusion that I need something else in my life. Seeing as I've always 
                 had an interest and passion for programming and computer in general, the choice was easy. 
                 So in january of 2023 I wrote my very first "Hello World"! 
-            </span>
+            </p>
 
-            <span>
+            <p>
                 From that point everything started to move very fast. I had to provide eligibility for most of the 
                 educations I had in mind, so the very first step was to enroll in an pre eligibility class for adults. 
                 I hadn't even gotten my grades when the big eligibility tests for the actual code Academy came around.
                 To actual pass the exams and get admitted did'nt feel as an possibility for me at the time.
                 But Lo and behold, I did!
-            </span>
-        </p>
+            </p>
+        
     </section>
 </div>
 
