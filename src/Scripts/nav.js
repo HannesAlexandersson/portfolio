cd2e092d793d3ac7955a69ad519a824f96d52335
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-03-27 20:52:58 +0100

    dd

--- src/Scripts/nav.js
+++ src/Scripts/nav.js
@@ -2,6 +2,7 @@
 const dropdownContent = document.getElementById('dropdownContent');  
 const navBar = document.querySelector('.navbar');
 const links = document.querySelector(".links-wrapper");
+const navBarMobile = document.querySelector('.navigation-mobile');
 
 // Scrolling navbar animation logic, makes it go away when user scrolls down and appear when user scrolls up
 let lastScrollTop = 0;
@@ -15,17 +16,25 @@ window.addEventListener('scroll', () => {
     if (scrolled > lastScrollTop) {
       navBar.classList.remove('sticky');
       navBar.classList.add('animate');
+
+      navBarMobile.classList.remove('sticky');
+      navBarMobile.classList.add('animate');
+      
       
     } else {
       navBar.classList.remove('animate');
       navBar.classList.add('sticky');
       
+      navBarMobile.classList.remove('animate');
+      navBarMobile.classList.add('sticky');
     }
   } else {
     navBar.classList.remove('animate', 'sticky');    
+    navBarMobile.classList.remove('animate', 'sticky');
   }
   
   lastScrollTop = scrolled;
 });
 
 
+ 
--- src/Views/partials/navbar.view.php
+++ src/Views/partials/navbar.view.php
@@ -14,7 +14,35 @@
     <a href="/contact" <?php echo ($_SERVER['REQUEST_URI'] == '/contact') ? 'class="active"' : ''; ?>>Contact Me<span></span></a>
   </div>
 
-  
- 
+
+  <!-- <div class="mobile"> -->
+    <div class="links navigation-mobile default-font"  id="dropdownContent">
+      <div class="nav-links-wrapper"><img src="/src/Assets/navIcons/home.svg"/><a href="/" <?php echo ($_SERVER['REQUEST_URI'] == '/') ? 'class="active"' : ''; ?>>Home<span></span></a></div>
+      <div class="nav-links-wrapper"><img src="/src/Assets/navIcons/collection.svg"/><a href="/collection" <?php echo ($_server['REQUEST_URI'] == 'collection') ? 'class="active"' : '';?>>Collection<span></span></a></div>
+      <div class="nav-links-wrapper"><img src="/src/Assets/navIcons/resume.svg"/><a href="/cv" <?php echo ($_SERVER['REQUEST_URI'] == '/cv') ? 'class="active"' : ''; ?>>CV<span></span></a></div>
+      <div class="nav-links-wrapper"><img src="/src/Assets/navIcons/story.svg"/><a href="/story" <?php echo ($_SERVER['REQUEST_URI'] == '/story') ? 'class="active"' : ''; ?>>My Story<span></span></a></div>
+      <div class="nav-links-wrapper"><img src="/src/Assets/navIcons/contact.svg"/><a href="/contact" <?php echo ($_SERVER['REQUEST_URI'] == '/contact') ? 'class="active"' : ''; ?>>Contact Me<span></span></a></div>
+    </div>
+
+    <div class="hamburger-menu" id="hamburgerMenu">
+      <div class="hamburger-icon" id="hamburgerIcon">&#9776;</div>
+    </div>
+  <!-- </div> -->
+
 <script src="/src/Scripts/nav.js"></script>
+<script>
+  
+  // JavaScript for toggling mobile menu
+  document.addEventListener('DOMContentLoaded', function() {
+
+    // JavaScript for toggling mobile menu
+ const hamburgerIcon = document.getElementById('hamburgerIcon');
+ const mobileMenu = document.querySelector('.navigation-mobile');
+ if (hamburgerIcon && mobileMenu) {
+      hamburgerIcon.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show');
+      });
+    }
+  });
+</script>
 </nav>
